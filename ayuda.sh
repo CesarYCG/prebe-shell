51ce63ee76732425e78a31d79f8596deed89d4d3
CesarYCG <42522481+CesarYCG@users.noreply.github.com>
2020-11-14 01:51:06 -0600

    script para ver todos los comandos.

---
+++ ayuda.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#Menu de ayuda que ofrece informacion sobre los comandos
+
+clear
+echo -e "
+
+\e[33;44m*----------------------------------------------------------------------*
+|                                                                      |
+| ||||||  ||||||  ||||  ||||    |||   ||||   || |||||   ||||||  |||||| |
+| ||      ||  ||  || |||| ||   |   |  || ||  || ||  ||  ||  ||  ||||   |
+| ||      ||  ||  ||  ||  ||  ||||||| ||  || || ||  ||  ||  ||     ||| |
+| ||||||  ||||||  ||      ||  ||   || ||   |||| |||||   ||||||  |||||| |
+|                                                                      |
+*----------------------------------------------------------------------*
+|             ||                                                       |
+|ayuda        || Te muestra los comandos posibles y una descripcion    |
+|             ||                                                       |
+|arbol        || Muestra la jerarquia de un directorio especifico      |
+|             ||                                                       |
+|infosis      || Informacion del poderoso sistema :O                   |
+|             ||                                                       |
+|fecha        || Muestra fecha y hora                                  |
+|             ||                                                       |
+|fechuza      || Muestra otra fecha y hora XD                          |
+|             ||                                                       |
+|buscar       || Busca un archivo, necesita el nombre del archivo y el |
+|             || directorio donde va a realizarse la busqueda :3       |
+|             ||                                                       |\e[0m
+\e[33;44m|prebeplayer  || Un reproductor musical manejado por prompt c:         |\e[0m
+\e[33;44m|             ||                                                       |\e[0m
+\e[33;44m|rockpaper    || Famoso juego de Piedra, Papel o Tijeras!              |\e[0m
+\e[33;44m|             ||                                                       |\e[0m
+\e[33;44m|creditos     || Muestra los nombres de los involucrados en este       |\e[0m
+\e[33;44m|             || bonito prebeshell :3                                  |\e[0m
+\e[33;44m|             ||                                                       |\e[0m
+\e[33;44m|salir        || Te conduce a la salida de este proyecto.              |\e[0m
+\e[33;44m|             || Gracias por pasarte, vuelve pronto!                   |\e[0m
+\e[33;44m|             ||                                                       |\e[0m
+\e[33;44m*----------------------------------------------------------------------*\e[0m
+"
+
+echo ""
