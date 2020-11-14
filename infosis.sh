ed0d06fb2ad9e97a4204a4104d33808a4beed790
CesarYCG <42522481+CesarYCG@users.noreply.github.com>
2020-11-14 04:34:52 -0600

    Infosis V2, información del sistema

--- infosis.sh
+++ infosis.sh
@@ -26,8 +26,10 @@ echo -n -e "\e[93mUsuario Conectado: \e[0m"
 whoami
 
 echo -e "\e[93mMemoria RAM disponible: \e[0m"
-free -m
+#free -m
+egrep 'MemTotal' /proc/meminfo
 echo -e "\e[93mArquitectura: \e[0m"
-uname -m
+#Extraemos del directorio la columna 2 y asi obtenemos la arquitectura
+file -Lb /usr/bin/ld | cut -d, -f2
 echo -e "\e[93mVersion del Sistema Operativo: \e[0m"
 cat /proc/version
