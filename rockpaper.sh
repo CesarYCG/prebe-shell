7d37cb9d2a63aa03a0c263c26dd810aaedc2387e
CesarYCG <42522481+CesarYCG@users.noreply.github.com>
2020-11-14 19:09:11 -0600

    Cambios de estilo

--- rockpaper.sh
+++ rockpaper.sh
@@ -19,16 +19,16 @@ sleep 2
  partidas=-1
  while true
  do
-  echo -n -e "\e[2;32mCuantas partidas quieres jugar:\e[2;32m "
+  echo -n -e "${style}Cuantas partidas quieres jugar:${stylef}"
   read -r partidas
   if ! [[ $partidas =~ $re ]] 
   then
-     echo -e "\e[2;32mNo es un numero valido de partidas\e[2;32m"
-     echo -e "\e[2;32mPorfavor intenta de nuevo\n\e[2;32m"
+     echo -e "${style}No es un numero valido de partidas${stylef}"
+     echo -e "${style}Porfavor intenta de nuevo\n${stylef}"
   elif [ $partidas -lt 0 ]; then
-    echo "\e[2;32mPDebes elegir al menos una ronda\e[2;32mP"
+    echo "\e${style}Debes elegir al menos una ronda${stylef}"
   else
-    echo -e "\e[95m$partidas partidas, es hora del duelo!\n\e[95m"
+    echo -e "${style}Elegiste $partidas partidas, es hora del duelo!\n${stylef}"
     break
   fi
 	done
@@ -36,66 +36,66 @@ sleep 2
 	
 while [ $partidas != 0 ]
 do
-echo -e "\e[95m\t\tPara jugar ingresaras solo el numero que corresponda a tu eleccion:\n\e[95m"
+echo -e "${style}\tPara jugar ingresaras solo el numero que corresponda a tu eleccion:\n${stylef}"
 aleatorio=$(( ( RANDOM % 3 )  + 1 ))
-echo -e "\e[31m1) Piedra\n\e[31m"
-echo -e "\e[32m2) Papel\n\e[32m"
-echo -e "\e[34m3) Tijeras\n\e[34m"  
+echo -e "${style}1) Piedra\n${stylef}"
+echo -e "${style}2) Papel\n${stylef}"
+echo -e "${style}3) Tijeras\n${stylef}"  
 read -e -p ">> " op
 case $op in
 	1)
-	echo -e "\e[31mElegiste Piedra\n\e[31m"
+	echo -e "${style}Elegiste Piedra\n${stylef}"
 	if [[ $aleatorio == "1" ]]
 		then
-		echo -e "\e[92m\tLa computadora elegio Piedra\n\n\e[92m"
-		echo -e "\e[95m\t\t\tEmpate\n\n\e[95m"
+		echo -e "${style}\tLa computadora elegio Piedra\n\n${stylef}"
+		echo -e "${style}\t\t\tEmpate\n\n${stylef}"
 	elif [[ $aleatorio == "2" ]]
 		then
-			echo -e "\e[93m\tLa computadora eligio Papel\n\n\e[93m"
-			echo -e "\e[95m\t\t\tGana la computadora\n\n\n\e[95m"
+			echo -e "${style}\tLa computadora eligio Papel\n\n${stylef}"
+			echo -e "${style}\t\t\tGana la computadora\n\n\n${stylef}"
 	elif [[ $aleatorio == "3" ]]
 		then
 		echo -e "\tLa computadora eligio Tijeras\n\n"
-		echo -e "\e[95m\t\t\tTu ganas\n\n\n\e[95m"
+		echo -e "${style}\t\t\tTu ganas\n\n\n${stylef}"
 	fi
 	partidas=$(($partidas -1))
 	;;
 	2)
-	echo -e "\e[32mElegiste Papel\n\e[32m"
+	echo -e "${style}Elegiste Papel\n${stylef}"
 	if [[ $aleatorio == "1" ]]
 		then
-		echo -e "\e[92m\tLa computadora eligio Piedra\n\n\e[92m"
-		echo -e "\e[95m\t\t\tTu ganas\n\n\n\e[95m"
+		echo -e "${style}\tLa computadora eligio Piedra\n\n${stylef}"
+		echo -e "${style}\t\t\tTu ganas\n\n\n${stylef}"
 	elif [[ $aleatorio == "2" ]]
 		then
-			echo -e "\e[93m\tLa computadora eligio Papel\n\n\e[93m"
-			echo -e "\e[95m\t\t\tEmpate\n\n\n\e[95m"
+			echo -e "${style}\tLa computadora eligio Papel\n\n${stylef}"
+			echo -e "${style}\t\t\tEmpate\n\n\n${stylef}"
 	elif [[ $aleatorio == "3" ]]
 		then 
-		echo -e "\e[96m\tLa computadora eligio Tijeras\n\n\e[96m"
-		echo -e "\e[95m\t\t\tGana la computadora\n\n\n\e[95m"
+		echo -e "${style}\tLa computadora eligio Tijeras\n\n${stylef}"
+		echo -e "${style}\t\t\tGana la computadora\n\n\n${stylef}"
 	fi
 	partidas=$(($partidas -1))
 	;;
 	3)
-	echo -e "\e[34mElegiste Tijeras\n\e[34m"
+	echo -e "${style}Elegiste Tijeras\n${stylef}"
 	if [[ $aleatorio == "1" ]]
 		then
-		echo -e "\e[92m\tLa computadora eligio Piedra\n\n\e[92m"
-		echo -e "\e[95m\t\t\tGana la computadora\n\n\n\e[95m"
+		echo -e "${style}\tLa computadora eligio Piedra\n\n${stylef}"
+		echo -e "${style}\t\t\tGana la computadora\n\n\n${stylef}"
 		
 	elif [[ $aleatorio == "2" ]]
 		then
-		echo -e "\e[93m\tLa computadora eligio Papel\n\n\e[93m"
-		echo -e "\e[95m\t\t\tTu ganas\n\n\n\e[95m"
+		echo -e "${style}\tLa computadora eligio Papel\n\n${stylef}"
+		echo -e "${style}\t\t\tTu ganas\n\n\n${stylef}"
 	elif [[ $aleatorio == "3" ]]
 		then
-		echo -e "\e[96m\tLa computadora eligio Tijeras\n\n\e[96m"
-		echo -e "\e[95m\t\t\tEmpate\n\n\n\e[95m"
+		echo -e "${style}\tLa computadora eligio Tijeras\n\n${stylef}"
+		echo -e "${style}\t\t\tEmpate\n\n\n${stylef}"
 	fi
 	partidas=$(($partidas -1))
 	;;
 	esac
 	done 
-	echo -e "\e[93m\t\tFin del juego, gracias por jugar :D !\e[93m" 
+	echo -e "${style}\t\tFin del juego, gracias por jugar :D !${stylef}" 
 	exit
