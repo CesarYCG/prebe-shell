53a65a99a2ab0e5313cca1f7125bbc271b0f1686
CesarYCG <42522481+CesarYCG@users.noreply.github.com>
2020-11-14 19:59:51 -0600

    Juego ahorcado

---
+++ ahorcado.sh
@@ -0,0 +1,36 @@
+#!bin/bash
+
+echo "Bienvenido a Ahorcado!!"
+pa=$((( $RANDOM%4 ) + 1 ))
+case $pa in
+1)op="Linux"
+echo "Kernel del sistema GNU/Linux"
+;;
+2)op="Linus Torvalds"
+echo "Creador del kernel Linux"
+;;
+3)op="Shell Script"
+echo "Lenguaje utilizado en la creacion de este programa"
+;;
+4)op="SSH"
+echo "Siglas de una shell segura"
+;;
+esac
+k="${op:0:1}"
+x=${#op}
+
+for n in $(seq 2 1 $x)
+do
+    k=$k" _"
+done
+echo ""
+echo "$k"
+intentos=3
+
+while [ "$xd" != "$op" ] && [ "$intentos" != "0" ]
+do
+echo "Te quedan $intentos intentos"
+let intentos--
+echo "$k"
+read -r xd
+done
